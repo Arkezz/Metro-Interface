72b66da691bee6c59cae5bfd2922686ea72fdcfd
= <40185507+Arkezz@users.noreply.github.com>
2023-03-21 04:04:05 +0200

    nicer ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <div class="hero">
   <a href="/">Home</a>
-  <a href="/login">Login</a>
-  <a href="/register">Register</a>
-  <a href="/password-reset">Reset Password</a>
   <a href="/admin">Admin Page</a>
+  <div class="dropdown">
+    <button class="dropbtn"><img src="profile.png" alt="Profile Icon" /></button
+    >
+    <div class="dropdown-content">
+      <a href="/login">Login</a>
+      <a href="/register">Register</a>
+      <a href="/password-reset">Reset Password</a>
+    </div>
+  </div>
 </div>
 
 <main>
@@ -16,14 +22,64 @@
 
 <style>
   @import url("https://fonts.cdnfonts.com/css/josefin-sans");
+
   * {
     margin: 0;
     padding: 0;
-    box-sizing: 0;
+    box-sizing: border-box;
     font-family: "Josefin Sans", sans-serif;
   }
 
   .hero {
-    font-size: large;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 1rem;
+    height: 4rem;
+    background-color: #fff;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  .hero a {
+    text-decoration: none;
+    color: #000;
+    font-size: 1.2rem;
+    margin-right: 1rem;
+  }
+
+  .dropdown {
+    position: relative;
+    display: inline-block;
+  }
+
+  .dropbtn {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    margin-left: 1rem;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    z-index: 1;
+    top: 3rem;
+    right: 0;
+    background-color: #fff;
+    min-width: 160px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
+  .dropdown-content a {
+    display: block;
+    text-decoration: none;
+    color: #000;
+    padding: 0.5rem;
+    font-size: 1rem;
+  }
+
+  .dropdown:hover .dropdown-content {
+    display: block;
   }
 </style>
