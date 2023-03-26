8b1c8d0a9e1d8bca1515b4fbdbfe20796244ffa0
= <40185507+Arkezz@users.noreply.github.com>
2023-03-26 23:05:25 +0200

    refactor

--- src/lib/components/passwordInput.svelte
+++ src/lib/components/passwordInput.svelte
@@ -4,21 +4,20 @@
   export let value = "";
   export let tooltip = true;
   export let isPasswordShort = false;
-  let requirements = "Password must be at least 8 characters long.";
   let show_password = false;
-  $: type = show_password ? "text" : "password";
 
   const handleInput = (event) => {
     const { value } = event.target;
     isPasswordShort = value.length >= 8;
-    const borderColor =
+    event.target.style.borderColor =
       value.length === 0 ? "#ccc" : isPasswordShort ? "green" : "red";
-    event.target.style.borderColor = borderColor;
     const passwordRequirements = document.querySelector(
       ".password-requirements"
     );
-    passwordRequirements.style.color = borderColor;
+    passwordRequirements.style.color = event.target.style.borderColor;
   };
+
+  $: type = show_password ? "text" : "password";
 </script>
 
 <div class="form-field">
@@ -41,7 +40,13 @@
     </button>
   </div>
   {#if tooltip}
-    <span class="password-requirements">{requirements}</span>
+    <span
+      class="password-requirements"
+      style="color: {isPasswordShort
+        ? 'green'
+        : '#ccc'}; font-size: 0.8rem; margin-top: 0.5rem;"
+      >Password must be at least 8 characters long.</span
+    >
   {/if}
 </div>
 
@@ -65,11 +70,5 @@
     display: block;
     margin-bottom: 0.5rem;
     font-size: 1.2rem;
-  }
-
-  .password-requirements {
-    color: #ccc;
-    font-size: 0.8rem;
-    margin-top: 0.5rem;
   }
 </style>
