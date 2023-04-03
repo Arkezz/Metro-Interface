96893481c6f950c372e5bf14514c65f969fc28cd
= <40185507+Arkezz@users.noreply.github.com>
2023-04-03 17:32:49 +0200

    yes

--- src/lib/components/auth/button.svelte
+++ src/lib/components/auth/button.svelte
@@ -1,11 +1,14 @@
+<!-- MyButton.svelte -->
 <script>
   export let text = "";
   export let isLoading = false;
 </script>
 
-<button aria-busy={isLoading}>
+<button aria-busy={isLoading ? "true" : "false"}>
   {#if !isLoading}
     {text}
+  {:else}
+    <span class="spinner" />
   {/if}
 </button>
 
@@ -21,11 +24,16 @@
     border-radius: 3px;
     cursor: pointer;
   }
-  button[aria-busy="true"]:after {
-    content: "";
+
+  button[aria-busy="true"] {
+    opacity: 0.5;
+    cursor: default;
+  }
+
+  .spinner {
     display: inline-block;
-    width: 0.8em;
-    height: 0.8em;
+    width: 1em;
+    height: 1em;
     border-radius: 50%;
     border: 0.2em solid rgba(255, 255, 255, 0.2);
     border-top-color: #fff;
