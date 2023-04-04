3640450dad23e80203900c0243315e08da78b1e6
= <40185507+Arkezz@users.noreply.github.com>
2023-04-04 12:40:30 +0200

    refactor

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import PlanRoute from "$lib/components/home/PlanRoute.svelte";
   import StationSearch from "$lib/components/home/stationSearch.svelte";
+  import Tickets from "$lib/components/home/tickets.svelte";
 
   export let data;
   $: ({ stations } = data);
@@ -16,34 +17,7 @@
     </div>
   </section>
 
-  <!-- Tickets section -->
-  <section class="tickets">
-    <h2>Buy tickets</h2>
-    <p>Choose from a range of ticket options to suit your needs.</p>
-    <div class="ticket-types">
-      <div class="ticket-type">
-        <i class="fas fa-calendar" />
-        <h3>Monthly pass</h3>
-        <p>Valid for one calendar month</p>
-        <p>Price: $30</p>
-        <a href="/buy/monthly" class="btn btn-primary">Buy now</a>
-      </div>
-      <div class="ticket-type">
-        <i class="fas fa-calendar-alt" />
-        <h3>Quarterly pass</h3>
-        <p>Valid for three calendar months</p>
-        <p>Price: $80</p>
-        <a href="/buy/quarterly" class="btn btn-primary">Buy now</a>
-      </div>
-      <div class="ticket-type">
-        <i class="fas fa-calendar-check" />
-        <h3>Annual pass</h3>
-        <p>Valid for one calendar year</p>
-        <p>Price: $250</p>
-        <a href="/buy/annual" class="btn btn-primary">Buy now</a>
-      </div>
-    </div>
-  </section>
+  <Tickets />
   <StationSearch {stations} />
   <PlanRoute />
 </main>
@@ -58,7 +32,7 @@
 
   /* Hero section */
   .hero {
-    height: 60vh;
+    height: 50vh;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -93,43 +67,5 @@
   .hero a:hover {
     background-color: #fff;
     color: #333;
-  }
-
-  /* Tickets section */
-  .tickets {
-    padding: 2rem 0;
-  }
-
-  .tickets h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-
-  .tickets p {
-    margin-bottom: 2rem;
-  }
-
-  .ticket-types {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .ticket-type {
-    flex-basis: 30%;
-    padding: 1rem;
-    background-color: #f9f9f9;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
-    text-align: center;
-  }
-
-  .ticket-type h3 {
-    font-size: 1.5rem;
-    margin-top: 1rem;
-  }
-
-  .ticket-type p {
-    margin-top: 0.5rem;
-    font-size: 1.2rem;
   }
 </style>
