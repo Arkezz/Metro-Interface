2258ac97967c8bd6f95e4c5c98018bbb619518ed
= <40185507+Arkezz@users.noreply.github.com>
2023-04-04 12:39:17 +0200

    small update

--- src/lib/components/home/tickets.svelte
+++ src/lib/components/home/tickets.svelte
@@ -33,221 +33,85 @@
 <style>
   /* Tickets section */
   .tickets {
-    padding: 2rem 0;
+    padding: 4rem 0;
+    text-align: center;
   }
 
   .tickets h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
+    font-size: 3.5rem;
+    margin-bottom: 2rem;
+    font-weight: bold;
+    color: #333;
   }
 
   .tickets p {
-    margin-bottom: 2rem;
+    margin-bottom: 4rem;
+    font-size: 1.5rem;
+    color: #666;
+    max-width: 60%;
+    margin-left: auto;
+    margin-right: auto;
   }
 
   .ticket-types {
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
+    flex-wrap: wrap;
   }
 
   .ticket-type {
-    flex-basis: 30%;
-    padding: 1rem;
-    background-color: #f9f9f9;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+    flex-basis: 20%;
+    padding: 2rem;
+    margin: 2rem;
+    background-color: #fff;
+    border-radius: 1rem;
+    box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
     text-align: center;
+    transition: all 0.3s ease-in-out;
   }
 
-  .ticket-type h3 {
-    font-size: 1.5rem;
-    margin-top: 1rem;
-  }
-
-  .ticket-type p {
-    margin-top: 0.5rem;
-    font-size: 1.2rem;
-  }
-
-  /* Stations section */
-  .stations {
-    padding: 2rem 0;
-  }
-
-  .stations h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-
-  .stations p {
-    margin-bottom: 2rem;
-  }
-
-  .station-search {
-    display: flex;
-    margin-bottom: 2rem;
-  }
-
-  .station-search input[type="text"] {
-    flex-grow: 1;
-    padding: 0.5rem;
-    font-size: 1.2rem;
-    border: 2px solid #ccc;
-    border-radius: 0.5rem 0 0 0.5rem;
-  }
-
-  .station-search button {
-    padding: 0.5rem 1rem;
-    font-size: 1.2rem;
-    background-color: #007bff;
-    color: #fff;
-    border: 2px solid #007bff;
-    border-radius: 0 0.5rem 0.5rem 0;
-    cursor: pointer;
-  }
-
-  .station-search button:hover {
-    background-color: #0069d9;
-    border-color: #0062cc;
-  }
-
-  .station-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 1rem;
-  }
-
-  .station {
-    background-color: #f9f9f9;
-    padding: 1rem;
-    border-radius: 0.5rem;
-    box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
-  }
-
-  .station h3 {
-    font-size: 1.5rem;
-    margin-top: 0;
-  }
-
-  .station p {
-    margin-top: 0.5rem;
-    font-size: 1.2rem;
-  }
-
-  /* Routes section */
-  .routes {
-    padding: 2rem 0;
-  }
-
-  .routes h2 {
-    font-size: 2rem;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 2rem;
-  }
-
-  /* Stations section */
-  .stations {
-    padding: 2rem 0;
-  }
-
-  .stations h2 {
-    font-size: 2rem;
-    margin-bottom: 1rem;
-  }
-
-  .stations p {
-    margin-bottom: 2rem;
-  }
-
-  .station-search {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 2rem;
-  }
-
-  .station-list {
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    grid-gap: 2rem;
-  }
-
-  .station {
-    padding: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-
-  .station h3 {
-    font-size: 1.5rem;
-    margin-bottom: 0.5rem;
-  }
-
-  .station p {
-    margin-bottom: 0.5rem;
-  }
-
-  /* Ticket types */
-  .ticket-types {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-top: 2rem;
-  }
-
-  .ticket-type {
-    flex-basis: calc(33.33% - 1rem);
-    padding: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+  .ticket-type:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.2);
   }
 
   .ticket-type i {
-    font-size: 3rem;
+    font-size: 5rem;
     margin-bottom: 1rem;
     display: block;
-    text-align: center;
+    color: #007bff;
   }
 
   .ticket-type h3 {
-    font-size: 1.5rem;
-    margin-bottom: 0.5rem;
-    text-align: center;
+    font-size: 2rem;
+    margin-bottom: 1.5rem;
+    font-weight: bold;
+    color: #333;
   }
 
   .ticket-type p {
-    margin-bottom: 0.5rem;
-    text-align: center;
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
+    color: #666;
   }
 
   /* Buttons */
   .btn {
-    padding: 0.5rem 1rem;
-    border-radius: 5px;
-    font-size: 1.2rem;
+    padding: 1rem 2rem;
+    border-radius: 5rem;
+    font-size: 1.5rem;
     font-weight: bold;
     cursor: pointer;
-    transition: all 0.2s ease-in-out;
+    transition: all 0.3s ease-in-out;
+    text-decoration: none;
+    border: 2px solid #007bff;
+    color: #007bff;
   }
 
-  .btn-primary {
-    color: #fff;
+  .btn:hover {
     background-color: #007bff;
-    border: none;
-  }
-
-  .btn-primary:hover {
-    background-color: #0069d9;
-  }
-
-  .btn-secondary {
     color: #fff;
-    background-color: #6c757d;
-    border: none;
-  }
-
-  .btn-secondary:hover {
-    background-color: #5a6268;
+    transform: translateY(-3px);
+    box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
   }
 </style>
