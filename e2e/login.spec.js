7149b8752e78ec10804edba7178bcf3b32543e47
= <40185507+Arkezz@users.noreply.github.com>
2023-04-08 21:06:07 +0200

    again

--- e2e/login.spec.js
+++ e2e/login.spec.js
@@ -1,15 +1,13 @@
-//playwright test e2e/login.spec.js --headed
+import { test, expect } from '@playwright/test';
 
-import { test } from '@playwright/test';
+test('the user can navigate to the home page and see the list of rows in a table', async ({
+  page,
+}) => {
+  await page.goto('/');
 
-test('login', async ({ page }) => {
-  await page.goto('/login');
-
-  await page.click('text=Login');
-  await page.fill('input[name="username"]', 'admin');
-  await page.fill('input[name="password"]', 'admin');
-  await page.click('text=Login');
-
-  //do something
-  await page.click('text=Logout');
+  // We check that the page has a table with at least 59 rows.
+  // We expect the table to have at least 59 rows because the default region is Africa for which
+  // the API returns 59 countries.
+  const tableRowsCount = await page.getByRole('table').getByRole('row').count();
+  expect(tableRowsCount).toBeGreaterThan(59);
 });
--- playwright.config.js
+++ playwright.config.js
@@ -71,8 +71,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run dev',
+    command: 'pnpm run build && pnpm run preview',
     url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 });
